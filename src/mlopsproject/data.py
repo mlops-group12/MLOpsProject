93d555395e5964e0cd52ca9d95e6b66c187d2201
jacob <jacob.borregaard@outlook.com>
2026-01-09 12:49:04 +0100

    updated model

--- src/mlopsproject/data.py
+++ src/mlopsproject/data.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 
 import numpy as np
-from torch.utils.data import Dataset, Subset
+from torch.utils.data import Subset, DataLoader
 from torchvision.datasets import CIFAR10
+from torchvision.transforms import transforms
 
 DATA_PATH = "data/"
 
-def get_data_splits(seed=0) -> tuple[Dataset, Dataset]: 
+def get_data_splits(seed=0, num_workers=9, train_batch_size=64) -> tuple[DataLoader, DataLoader, DataLoader]: 
     cifar10_path = Path(DATA_PATH) / "cifar-10"
+
+    transform = transforms.Compose([transforms.ToTensor()])
 
     if not cifar10_path.exists():
         cifar10_path.mkdir()
@@ -19,14 +22,42 @@ def get_data_splits(seed=0) -> tuple[Dataset, Dataset]:
     validation_indices = indices[40000:]
 
 
-    main_data = CIFAR10(root=cifar10_path, train=True, download=True, transform=None)
+    main_data = CIFAR10(root=cifar10_path, train=True, download=True, transform=transform)
 
     train_data = Subset(main_data, train_indices)
     validation_data = Subset(main_data, validation_indices)
-    
-    test_data = CIFAR10(root=cifar10_path, train=False, download=True, transform=None)
 
-    return train_data, validation_data, test_data
+    test_data = CIFAR10(
+        root=cifar10_path,
+        train=False,
+        download=True,
+        transform=transform
+    )
+
+    # ------------------------------------------------------------------
+    # convert to dataloaders:
+    # ------------------------------------------------------------------
+
+    train_dataloader = DataLoader(
+        train_data,
+        num_workers=num_workers,
+        batch_size=train_batch_size,
+        persistent_workers=True,
+    )
+
+    test_dataloader = DataLoader(
+        test_data,
+        num_workers=num_workers,
+        persistent_workers=True,
+    )
+
+    validation_dataloader = DataLoader(
+        validation_data,
+        num_workers=num_workers,
+        persistent_workers=True,
+    )
+
+    return train_dataloader, test_dataloader, validation_dataloader
 
 if __name__ == "__main__":
     train_data, test_data = get_data_splits()
