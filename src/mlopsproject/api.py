8fdbfb1c1f39dadd34681e37d993c4404fad23b2
SigneDjernis <signedjernis@gmail.com>
2026-01-16 10:01:16 +0100

    added api for model

--- src/mlopsproject/api.py
+++ src/mlopsproject/api.py
@@ -1,1 +1,39 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import FileResponse
+from PIL import Image
+import torch
+from mlopsproject.model import CNN
+import sys
+import numpy as np
+
+app = FastAPI()
+
+@app.post("/predict/")
+async def predict(data: UploadFile = File(...)):
+    """Predict emotions for an image."""
+    i_image = Image.open(data.file)
+    if i_image.mode != "L": # RGB to greyscale conversion
+        i_image = i_image.convert(mode="L")
+
+    res = i_image.resize((64, 64))
+
+    model = CNN()
+
+    # setup WandB logger only if enabled
+    try:
+        model.load_state_dict(torch.load("../../models/model_weights_latest.pt"))
+    except FileNotFoundError:
+        print("Model weights not found!")
+        sys.exit(0)
+    
+    model.eval()
+    with torch.no_grad():
+        img_np = np.array(res, dtype=np.float32)
+        tensor_image = torch.from_numpy(img_np).unsqueeze(0).unsqueeze(0)
+        output = model(tensor_image)
+        predicted = torch.argmax(output.squeeze())
+
+    class_names = ["angry", "fear", "happy", "sad", "surprise"]
+
+    return {"predicted_emotion": class_names[predicted.item()]}
 
