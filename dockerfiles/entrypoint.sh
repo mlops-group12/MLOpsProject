dc7ad22a3eb4e540c929254d6a41087645e781be
jacob <jacob.borregaard@outlook.com>
2026-01-22 16:18:44 +0100

    update

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 cd /app
 export DVC_NO_SCM=1
 
-dvc pull data/train_data
+dvc --no-scm pull data/train_data
 # dvc pull data/celeba   # uncomment if you need it too
 
 exec uv run python -m src.mlopsproject.train wandb.enabled=false
